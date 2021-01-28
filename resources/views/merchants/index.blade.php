eea4dcdd49ea027654ff8ddc0a1a4fb75b8c110d
Mohamed Sayed Salem <mohamed.salem@egicons.com>
2021-01-28 19:32:15 +0200

    padding

--- resources/views/merchants/index.blade.php
+++ resources/views/merchants/index.blade.php
@@ -55,9 +55,10 @@
             </div>
         </div>
     </div>
-    <div class="row">
+    <div class="row" >
+        <div class="col-xl-4">
 
-        <div class="card col-4">
+        <div class="card " >
             <div class="card-header header-elements-inline">
                 <h6 class="card-title"> @lang('merchantDashbaord.last_categories') </h6>
 
@@ -87,7 +88,10 @@
                 </ul>
             </div>
         </div>
-    <div class="card col-4">
+        </div>
+        <div class="col-xl-4">
+
+        <div class="card">
             <div class="card-header header-elements-inline">
                 <h6 class="card-title"> @lang('merchantDashbaord.last_products') </h6>
 
@@ -120,7 +124,10 @@
                 </ul>
             </div>
         </div>
- <div class="card col-4">
+        </div>
+        <div class="col-xl-4">
+
+        <div class="card">
             <div class="card-header header-elements-inline">
                 <h6 class="card-title"> @lang('merchantDashbaord.last_orders') </h6>
 
@@ -153,281 +160,9 @@
                 </ul>
             </div>
         </div>
-
+        </div>
     </div>
 
-    {{--	<div class="row">--}}
-
-
-
-{{-- 		<div class="col-xl-3">--}}
-{{--			<div class="card">--}}
-{{--				<div class="card-header bg-transparent header-elements-inline">--}}
-{{--					<span class="card-title font-weight-semibold"> احصائيات السائقين </span>--}}
-{{--					<div class="header-elements">--}}
-{{--						<div class="list-icons">--}}
-{{--							<a class="list-icons-item" data-action="collapse"></a>--}}
-{{--						</div>--}}
-{{--					</div>--}}
-{{--				</div>--}}
-
-{{--				<div class="card-body">--}}
-{{--					<ul class="list-unstyled mb-0">--}}
-{{--						<li class="mb-3">--}}
-{{--							<div class="d-flex align-items-center mb-1"> اجمالى السائقين <span class="text-muted ml-auto">--}}
-{{--								{{ App\Models\Driver::count() }} سائق--}}
-{{--							</span></div>--}}
-{{--							<div class="progress" style="height: 0.125rem;">--}}
-{{--								<div class="progress-bar bg-primary" style="width: 50%">--}}
-{{--									<span class="sr-only">50% Complete</span>--}}
-{{--								</div>--}}
-{{--							</div>--}}
-{{--						</li>--}}
-
-
-{{--						<li class="mb-3">--}}
-{{--							<div class="d-flex align-items-center mb-1"> جاهز للعمل <span class="text-muted ml-auto">--}}
-{{--								1--}}
-{{--							</span></div>--}}
-{{--							<div class="progress" style="height: 0.125rem;">--}}
-{{--								<div class="progress-bar bg-success" style="width: 50%">--}}
-{{--									<span class="sr-only">50% Complete</span>--}}
-{{--								</div>--}}
-{{--							</div>--}}
-{{--						</li>--}}
-
-
-{{--						<li class="mb-3">--}}
-{{--							<div class="d-flex align-items-center mb-1">مشغول <span class="text-muted ml-auto">--}}
-{{--								1--}}
-{{--							</span></div>--}}
-{{--							<div class="progress" style="height: 0.125rem;">--}}
-{{--								<div class="progress-bar bg-warning" style="width: 50%">--}}
-{{--									<span class="sr-only">50% Complete</span>--}}
-{{--								</div>--}}
-{{--							</div>--}}
-{{--						</li>--}}
-
-
-{{--						<li class="mb-3">--}}
-{{--							<div class="d-flex align-items-center mb-1"> غير متاح  <span class="text-muted ml-auto">--}}
-{{--								1--}}
-{{--							</span></div>--}}
-{{--							<div class="progress" style="height: 0.125rem;">--}}
-{{--								<div class="progress-bar bg-danger" style="width: 50%">--}}
-{{--									<span class="sr-only">50% Complete</span>--}}
-{{--								</div>--}}
-{{--							</div>--}}
-{{--						</li>--}}
-
-
-
-
-
-{{--					</ul>--}}
-{{--				</div>--}}
-{{--			</div>--}}
-{{--		</div>--}}
-
-
-
-{{--		<div class="col-xl-3">--}}
-{{--			<div class="card">--}}
-{{--				<div class="card-header bg-transparent header-elements-inline">--}}
-{{--					<span class="card-title font-weight-semibold"> احصائيات الرحلات</span>--}}
-{{--					<div class="header-elements">--}}
-{{--						<div class="list-icons">--}}
-{{--							<a class="list-icons-item" data-action="collapse"></a>--}}
-{{--						</div>--}}
-{{--					</div>--}}
-{{--				</div>--}}
-
-{{--				<div class="card-body">--}}
-{{--					<ul class="list-unstyled mb-0">--}}
-{{--						<li class="mb-3">--}}
-{{--							<div class="d-flex align-items-center mb-1"> اجمالى رحلات اليوم <span class="text-muted ml-auto">--}}
-{{--								50--}}
-{{--							</span></div>--}}
-{{--							<div class="progress" style="height: 0.125rem;">--}}
-{{--								<div class="progress-bar bg-primary" style="width: 50%">--}}
-{{--									<span class="sr-only">50% Complete</span>--}}
-{{--								</div>--}}
-{{--							</div>--}}
-{{--						</li>--}}
-
-
-{{--						<li class="mb-3">--}}
-{{--							<div class="d-flex align-items-center mb-1"> الرحلات التى تم توصيلها <span class="text-muted ml-auto">22</span></div>--}}
-{{--							<div class="progress" style="height: 0.125rem;">--}}
-{{--								<div class="progress-bar bg-success-400" style="width: 80%">--}}
-{{--									<span class="sr-only">80% Complete</span>--}}
-{{--								</div>--}}
-{{--							</div>--}}
-{{--						</li>--}}
-
-
-{{--						<li class="mb-3">--}}
-{{--							<div class="d-flex align-items-center mb-1"> الرحلات قيد التنفيذ <span class="text-muted ml-auto">10</span></div>--}}
-{{--							<div class="progress" style="height: 0.125rem;">--}}
-{{--								<div class="progress-bar bg-warning" style="width: 70%">--}}
-{{--									<span class="sr-only">70% Complete</span>--}}
-{{--								</div>--}}
-{{--							</div>--}}
-{{--						</li>--}}
-
-{{--						<li class="mb-3">--}}
-{{--							<div class="d-flex align-items-center mb-1"> الرحلات قيد التوصيل <span class="text-muted ml-auto">22</span></div>--}}
-{{--							<div class="progress" style="height: 0.125rem;">--}}
-{{--								<div class="progress-bar bg-success-400" style="width: 80%">--}}
-{{--									<span class="sr-only">80% Complete</span>--}}
-{{--								</div>--}}
-{{--							</div>--}}
-{{--						</li>--}}
-
-{{--						<li>--}}
-{{--							<div class="d-flex align-items-center mb-1"> الرحلات الملغيه <span class="text-muted ml-auto">--}}
-{{--								2--}}
-{{--							</span></div>--}}
-{{--							<div class="progress" style="height: 0.125rem;">--}}
-{{--								<div class="progress-bar bg-grey-400" style="width: 60%">--}}
-{{--									<span class="sr-only">60% Complete</span>--}}
-{{--								</div>--}}
-{{--							</div>--}}
-{{--						</li>--}}
-{{--					</ul>--}}
-{{--				</div>--}}
-{{--			</div>--}}
-{{--		</div>--}}
-
-
-
-{{--		<div class="col-xl-3">--}}
-{{--			<div class="card">--}}
-{{--				<div class="card-header bg-transparent header-elements-inline">--}}
-{{--					<span class="card-title font-weight-semibold">  الاحصائيات الماليه </span>--}}
-{{--					<div class="header-elements">--}}
-{{--						<div class="list-icons">--}}
-{{--							<a class="list-icons-item" data-action="collapse"></a>--}}
-{{--						</div>--}}
-{{--					</div>--}}
-{{--				</div>--}}
-
-{{--				<div class="card-body">--}}
-{{--					<ul class="list-unstyled mb-0">--}}
-{{--						<li class="mb-3">--}}
-{{--							<div class="d-flex align-items-center mb-1"> اجمالى اراد اليوم <span class="text-muted ml-auto">5500 دينار</span></div>--}}
-{{--							<div class="progress" style="height: 0.125rem;">--}}
-{{--								<div class="progress-bar bg-danger" style="width: 50%">--}}
-{{--									<span class="sr-only">50% Complete</span>--}}
-{{--								</div>--}}
-{{--							</div>--}}
-{{--						</li>--}}
-
-{{--						<li class="mb-3">--}}
-{{--							<div class="d-flex align-items-center mb-1"> اجمالى التوصيل من الكاش <span class="text-muted ml-auto"> 500 دينار </span></div>--}}
-{{--							<div class="progress" style="height: 0.125rem;">--}}
-{{--								<div class="progress-bar bg-warning-400" style="width: 70%">--}}
-{{--									<span class="sr-only">70% Complete</span>--}}
-{{--								</div>--}}
-{{--							</div>--}}
-{{--						</li>--}}
-
-{{--						<li class="mb-3">--}}
-{{--							<div class="d-flex align-items-center mb-1">  اجمالى ايراد التوصيل من الكى نت <span class="text-muted ml-auto"> 1000 دينار  </span></div>--}}
-{{--							<div class="progress" style="height: 0.125rem;">--}}
-{{--								<div class="progress-bar bg-success-400" style="width: 80%">--}}
-{{--									<span class="sr-only">80% Complete</span>--}}
-{{--								</div>--}}
-{{--							</div>--}}
-{{--						</li>--}}
-
-
-{{--						<li class="mb-3">--}}
-{{--							<div class="d-flex align-items-center mb-1"> اجمالى التوصيل  <span class="text-muted ml-auto"> 1500 دينار  </span></div>--}}
-{{--							<div class="progress" style="height: 0.125rem;">--}}
-{{--								<div class="progress-bar bg-success-400" style="width: 80%">--}}
-{{--									<span class="sr-only">80% Complete</span>--}}
-{{--								</div>--}}
-{{--							</div>--}}
-{{--						</li>--}}
-
-
-{{--						<li class="mb-3">--}}
-{{--							<div class="d-flex align-items-center mb-1"> المستحق دفع للمتجر  <span class="text-muted ml-auto"> 4000 دينار  </span></div>--}}
-{{--							<div class="progress" style="height: 0.125rem;">--}}
-{{--								<div class="progress-bar bg-success-400" style="width: 80%">--}}
-{{--									<span class="sr-only">80% Complete</span>--}}
-{{--								</div>--}}
-{{--							</div>--}}
-{{--						</li>--}}
-
-
-{{--					</ul>--}}
-{{--				</div>--}}
-{{--			</div>--}}
-{{--		</div>--}}
-{{-- --}}
-
-{{--		<div class="col-sm-6 col-xl-3">--}}
-{{--			<div class="card card-body">--}}
-{{--				<div class="media">--}}
-{{--					<div class="mr-3 align-self-center">--}}
-{{--						<i class="icon-pointer icon-3x text-success-400"></i>--}}
-{{--					</div>--}}
-
-{{--					<div class="media-body text-right">--}}
-{{--						<h3 class="font-weight-semibold mb-0">120</h3>--}}
-{{--						<span class="text-uppercase font-size-sm text-muted"> طلب توصيل </span>--}}
-{{--					</div>--}}
-{{--				</div>--}}
-{{--			</div>--}}
-{{--		</div>--}}
-
-{{--		<div class="col-sm-6 col-xl-3">--}}
-{{--			<div class="card card-body">--}}
-{{--				<div class="media">--}}
-{{--					<div class="mr-3 align-self-center">--}}
-{{--						<i class="icon-enter6 icon-3x text-indigo-400"></i>--}}
-{{--					</div>--}}
-
-{{--					<div class="media-body text-right">--}}
-{{--						<h3 class="font-weight-semibold mb-0">5</h3>--}}
-{{--						<span class="text-uppercase font-size-sm text-muted"> مشرفين </span>--}}
-{{--					</div>--}}
-{{--				</div>--}}
-{{--			</div>--}}
-{{--		</div>--}}
-
-{{--		<div class="col-sm-6 col-xl-3">--}}
-{{--			<div class="card card-body">--}}
-{{--				<div class="media">--}}
-{{--					<div class="media-body">--}}
-{{--						<h3 class="font-weight-semibold mb-0">3</h3>--}}
-{{--						<span class="text-uppercase font-size-sm text-muted">   فروع </span>--}}
-{{--					</div>--}}
-
-{{--					<div class="ml-3 align-self-center">--}}
-{{--						<i class="icon-bubbles4 icon-3x text-blue-400"></i>--}}
-{{--					</div>--}}
-{{--				</div>--}}
-{{--			</div>--}}
-{{--		</div>--}}
-
-{{--		<div class="col-sm-6 col-xl-3">--}}
-{{--			<div class="card card-body">--}}
-{{--				<div class="media">--}}
-{{--					<div class="media-body">--}}
-{{--						<h3 class="font-weight-semibold mb-0">500 ريال </h3>--}}
-{{--						<span class="text-uppercase font-size-sm text-muted"> المبلغ المتوفر لك </span>--}}
-{{--					</div>--}}
-
-{{--					<div class="ml-3 align-self-center">--}}
-{{--						<i class="icon-bag icon-3x text-danger-400"></i>--}}
-{{--					</div>--}}
-{{--				</div>--}}
-{{--			</div>--}}
-{{--		</div>--}}
-{{--	</div>--}}
 
 </div>
 
