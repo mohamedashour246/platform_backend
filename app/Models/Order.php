3639ebc7ee01cde0972f687e75592ba2939b8c8f
mohamed <mohamedarafa274@gmail.com>
2021-01-28 18:35:03 +0200

    database cart

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -7,6 +7,34 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
+
+    public function currentStatus(){
+        $status = "";
+        if (app()->getLocale()== 'en'){
+            if ($this->order_status == 'new'){
+                $status= 'New';
+            }
+            elseif ($this->order_status == 'in_progress'){
+                $status= 'In Progress';
+            }
+            elseif ($this->order_status == 'delivered'){
+                $status= 'delivered';
+            }
+        }
+
+        if (app()->getLocale()== 'ar'){
+            if ($this->order_status == 'new'){
+                $status= 'جديد';
+            }
+            elseif ($this->order_status == 'in_progress'){
+                $status= 'قيد التحضير';
+            }
+            elseif ($this->order_status == 'delivered'){
+                $status= 'تم التوصيل';
+            }
+        }
+        return $status;
+    }
     public function owner() {
         return $this->belongsTo(User::class , 'client_id');
     }
